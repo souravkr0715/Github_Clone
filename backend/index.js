c666d2b69ee169e8665b91df58eaed62c804d596
Sourav Kumar <souravkumar@Souravs-MacBook-Air.local>
2026-03-22 08:41:58 +0530

    Added backend code properly

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const { initRepo } = require("./controllers/init");
+const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
+
+yargs(hideBin(process.argv))
+.command("start", "starts a new server", {}, startServer)
+  .command("init", "Initialise a new repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "add a file to the repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to add to the staging area",
+        type: "string",
+      });
+    },
+    (argv) => {
+      addRepo(argv.file);
+    },
+  )
+  .command(
+    "commit <message>",
+    "commit the staged files",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
+      });
+    },
+    (argv) => {
+      commitRepo(argv.message);
+    },
+  )
+  .command("push", "push commit to s3", {}, pushRepo)
+  .command("pull", "pull commit to s3", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revert to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "Commit ID to revert to",
+        type: "string",
+      });
+    },
+     (argv) => {
+      revertRepo(argv.commitID);
+    },
+  )
+  .demandCommand(1, "You need at least one command")
+  .help().argv;
+
+
+
+  function startServer(){
+    console.log("Server logic called");
+  }
